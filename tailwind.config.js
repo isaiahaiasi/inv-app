ceaf16d4033cbd48bbb706da5ae7dcf1a223b112
Isaiah <computerblender@gmail.com>
2021-05-26 00:37:13 -0700

    Add Tailwind CSS to project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    // TODO: add paths to purge, for production
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
